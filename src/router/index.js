b7697767d548711b0f176230752291477ee53a07
Marvin Sanchez <m4rvin2005@gmail.com>
2018-02-24 09:09:31 +0800

    add bare pages, levelbar

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/pages/HelloWorld';
+
+import Dashboard from '@/pages/Dashboard';
+import Posts from '@/pages/Posts';
 
 Vue.use(Router);
 
@@ -8,8 +10,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Dashboard',
+      component: Dashboard,
+    },
+    {
+      path: '/posts',
+      name: 'Posts',
+      component: Posts,
     },
   ],
 });
