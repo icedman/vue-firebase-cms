2364c702ea0c95030edbf663e711965b26bad97e
Clint James <cjestal@gmail.com>
2018-02-25 00:09:42 +0800

    added config

---
+++ config/config.js
@@ -0,0 +1,10 @@
+{
+  "firebase": {
+    "apiKey": "AIzaSyCHIgoOPVJvlABRtJEp_LPweNceVRrak-k",
+    "authDomain": "appsmart-contracts.firebaseapp.com",
+    "databaseURL": "https://appsmart-contracts.firebaseio.com",
+    "projectId": "appsmart-contracts",
+    "storageBucket": "appsmart-contracts.appspot.com",
+    "messagingSenderId": "178298224563"
+  }
+}
