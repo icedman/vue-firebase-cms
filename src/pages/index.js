73b1fdef6964ced5f556ee2a35b3ba8f0217591a
Marvin Sanchez <m4rvin2005@gmail.com>
2018-02-25 01:00:28 +0800

    add login, register to component index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 export { default as Dashboard } from './Dashboard';
 export { default as Pages } from './Pages';
 export { default as Posts } from './Posts';
+export { default as Login } from './Login';
+export { default as Register } from './Register';
 export { default as TemplatePage } from './TemplatePage';
